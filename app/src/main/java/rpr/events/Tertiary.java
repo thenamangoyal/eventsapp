f705bfe7c0e57fc09f694430ec19e46be8fee342
Vishal Singh <vishalsingh020997@gmail.com>
2017-04-03 18:13:59 +0530

    database

--- app/src/main/java/rpr/events/Tertiary.java
+++ app/src/main/java/rpr/events/Tertiary.java
@@ -5,8 +5,10 @@ package rpr.events;
  */
 
 import android.app.ProgressDialog;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -20,6 +22,7 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
+import android.database.Cursor;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -29,7 +32,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
+
 public class Tertiary extends Fragment {
+
+    public SQLiteDatabase db;
 
     private static Context context = null;
     private static final String TAG = Menu3.class.getSimpleName();
@@ -53,8 +59,18 @@ public class Tertiary extends Fragment {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
         context = getActivity();
-        return inflater.inflate(R.layout.primary_layout, container, false);
+        createDatabase();
+        return inflater.inflate(R.layout.tertiary_layout, container, false);
 
+    }
+    protected void createDatabase(){
+        db=getActivity().openOrCreateDatabase("EventDB", Context.MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS Events(event_id INTEGER NOT NULL, name VARCHAR NOT NULL,venue VARCHAR NOT NULL, time TIMESTAMP NOT NULL, details VARCHAR NOT NULL  );");
+    }
+
+    protected void insertIntoDB(String Event_id, String Name, String Time, String Venue, String Details){
+        String query = "INSERT INTO Events(event_id,name,venue, time, details) VALUES('"+Event_id+"', '"+Name+"', '"+Time+"', '"+Venue+"', '"+Details+"');";
+        db.execSQL(query);
     }
 
 
@@ -70,7 +86,6 @@ public class Tertiary extends Fragment {
         //String ListURL = "http://10.1.1.19/~2015csb1021/php/ListAll.php";
 
         eventList = new ArrayList<>();
-
         lv = (ListView) getView().findViewById(R.id.listView);
 
         new Tertiary.Getevents().execute();
@@ -104,12 +119,11 @@ public class Tertiary extends Fragment {
                 try {
                     JSONObject jsonObj = new JSONObject(jsonStr);
 
-//                    Log.e("OnCreateView", "6");
-                    // Getting JSON Array node
+
                     JSONArray events = jsonObj.getJSONArray("events");
 
-                    //                  Log.e("OnCreateView", "6");
-                    // looping through All events
+
+                    db.execSQL("DELETE FROM Events");
                     for (int i = 0; i < events.length(); i++) {
                         JSONObject c = events.getJSONObject(i);
 
@@ -118,6 +132,8 @@ public class Tertiary extends Fragment {
                         String time = c.getString(TAG_TIME);
                         String venue = c.getString(TAG_VENUE);
                         String details = c.getString(TAG_DETAILS);
+
+                        insertIntoDB(event_id, name, time, venue, details);
 
                         // tmp hash map for single event
                         HashMap<String, String> event = new HashMap<>();
@@ -132,6 +148,7 @@ public class Tertiary extends Fragment {
                         // adding event to event list
                         eventList.add(event);
                     }
+                    db.close();
 
                 } catch (final JSONException e) {
                     Log.e(TAG, "Json parsing error: " + e.getMessage());
@@ -153,11 +170,53 @@ public class Tertiary extends Fragment {
                     @Override
                     public void run() {
                         Toast.makeText(context.getApplicationContext(),
-                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                "No Network",
                                 Toast.LENGTH_LONG)
                                 .show();
+
                     }
                 });
+
+
+
+                String query = "SELECT * FROM Events WHERE 1";
+                //Cursor points to a location in your results
+                Cursor recordSet = db.rawQuery(query, null);
+                //Move to the first row in your results
+                recordSet.moveToFirst();
+
+
+                //Position after the last row means the end of the results
+                while (!recordSet.isAfterLast()) {
+
+
+                    String event_id = recordSet.getString(recordSet.getColumnIndex("event_id"));
+                    String name = recordSet.getString(recordSet.getColumnIndex("name"));
+                    String time = recordSet.getString(recordSet.getColumnIndex("time"));
+                    String venue = recordSet.getString(recordSet.getColumnIndex("venue"));
+                    String details = recordSet.getString(recordSet.getColumnIndex("details"));
+
+
+                    HashMap<String, String> event = new HashMap<>();
+
+                    // adding each child node to HashMap key => value
+                    event.put(TAG_EVENT_ID, event_id);
+                    event.put(TAG_NAME, name);
+                    event.put(TAG_TIME, time);
+                    event.put(TAG_VENUE, venue);
+                    event.put(TAG_DETAILS, details);
+
+                    // adding event to event list
+                    eventList.add(event);
+
+
+                    recordSet.moveToNext();
+
+                }
+
+                db.close();
+
+
 
             }
 
